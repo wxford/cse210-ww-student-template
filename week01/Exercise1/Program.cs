b4e08f62bd7592b54218a68e4de51ad04ac2b9a8
Bockarie Junior Mansaray <mansarayjunior3@gmail.com>
2025-05-08 13:24:52 -0700

    week1 assignment

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -10,6 +10,6 @@ class Program
         Console.Write("What is your last name? ");
         string lastName = Console.ReadLine();
 
-        Console.WriteLine($"Your name is {lastName}, {firstName} {lastName}.");
+        Console.WriteLine($"Your name is. {firstName} {lastName}.");
     }
 }
